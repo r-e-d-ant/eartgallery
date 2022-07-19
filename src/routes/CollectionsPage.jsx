6cc2bfb154bf6fc774423bdf6e5954e42b426b05
r-e-d-ant <mugishathierry65@gmail.com>
2022-07-19 16:20:41 +0200

    search feature implemented

--- src/routes/CollectionsPage.jsx
+++ src/routes/CollectionsPage.jsx
@@ -5,10 +5,15 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const CollectionsPage = ({setDarkBg}) => {
+    const [searchData, setSearchData] = useState('');
     const [currentUrl, setCurrentUrl] = useState('https://api.artic.edu/api/v1/artworks?page=1&limit=12&fields=id,api_link,title,artist_title,image_id,publication_history,place_of_origin,date_display,copyright_notice');
     const {data: arts, isPending, error} = useFetch(`${currentUrl}`);
 
     const location = useLocation();
+
+    const handleSearchData = () => {
+        setCurrentUrl(`https://api.artic.edu/api/v1/artworks/search?q=${searchData}&limit=100&fields=id,api_link,title,artist_title,image_id,publication_history,place_of_origin,date_display,copyright_notice`);
+    }
 
     // go to the top of the page everytime when going on this route
     useEffect(() => {
@@ -45,8 +50,13 @@ const CollectionsPage = ({setDarkBg}) => {
                 </div>
                 <form className="search-form">
                     <div className="form-control">
-                        <input type="search" placeholder="Search by keyword, artist" className="search-input"></input>
-                        <div className="icon-container search-btn">
+                        <input
+                            type="search"
+                            placeholder="Search by keyword, artist"
+                            className="search-input"
+                            onChange={(e) => setSearchData(e.target.value)}
+                            ></input>
+                        <div className="icon-container search-btn" onClick={handleSearchData}>
                             <i className='bx bx-search-alt-2'></i>
                         </div>
                     </div>
@@ -60,8 +70,8 @@ const CollectionsPage = ({setDarkBg}) => {
             </section>
 
             <div className="container nav-btns-container">
-                {arts && <button onClick={() => prevPage(arts.pagination.prev_url)} disabled={arts.pagination.current_page <= 1 ? true : false}>prev</button>}
-                {arts && <button onClick={() => nextPage(arts.pagination.next_url)} disabled={arts.pagination.current_page === arts.pagination.total_pages ? true : false }>next</button>}
+                {arts && arts.pagination.prev_url && <button onClick={() => prevPage(arts.pagination.prev_url)} disabled={arts.pagination.current_page <= 1 ? true : false}>prev</button>}
+                {arts && arts.pagination.next_url && <button onClick={() => nextPage(arts.pagination.next_url)} disabled={arts.pagination.current_page === arts.pagination.total_pages ? true : false }>next</button>}
             </div>
         </main>
      );
